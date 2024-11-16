016859f21f2b78ab15a7d59cc1015f1478b876b5
Aryav <aryavbhola@gmail.com>
2024-11-17 01:18:15 +0530

    updated CharacterDetails and CharacterList components to display information in a better way

--- frontend/src/components/CharacterDetails.tsx
+++ frontend/src/components/CharacterDetails.tsx
@@ -1,23 +1,60 @@
 import React from "react";
-import { useEpisodes } from "../hooks/useEpisodes";
+import { Link, useParams } from "react-router-dom";
+import { useCharacter } from "../hooks/useCharacter";
 
-const CharacterDetails: React.FC<{ characterName: string }> = ({ characterName }) => {
-	const { data, isLoading, error } = useEpisodes(characterName);
+const CharacterDetails: React.FC = () => {
+	const { name } = useParams();
+	const { data: character, isLoading, error } = useCharacter(name as string);
 
-	if (isLoading) return <p className='text-center text-gray-500'>Loading episodes...</p>;
-	if (error) return <p className='text-center text-red-500'>Error fetching episodes.</p>;
+	if (character) console.log(character);
+
+	if (isLoading) return <p className='text-center text-gray-500'>Loading details...</p>;
+	if (error) return <p className='text-center text-red-500'>Error loading details.</p>;
 
 	return (
-		<div className='p-4'>
-			<h2 className='text-2xl font-bold text-center'>{data.character}</h2>
-			<ul className='mt-4 space-y-2'>
-				{data.episodes.map((episode: number) => (
-					<li key={episode} className='p-2 bg-gray-100 rounded shadow'>
-						Episode {episode}
-					</li>
-				))}
-			</ul>
-		</div>
+		<>
+			<Link
+				to='/'
+				className='my-4 px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600'>
+				Back to List
+			</Link>
+
+			<div className='grid grid-cols-2 gap-4 bg-gray-100 p-6 rounded-lg shadow-md justify-items-center'>
+				<div className='flex justify-around w-full gap-4'>
+					<div>
+						<h2 className='text-2xl font-bold mb-4'>{character.name}</h2>
+						<p className='text-gray-700'>ID: {character.id}</p>
+						<p className='text-gray-700'>Status: {character.status}</p>
+						<p className='text-gray-700'>Species: {character.species}</p>
+						<p className='text-gray-700'>Type: {character.type}</p>
+						<p className='text-gray-700'>Gender: {character.gender}</p>
+						<p className='text-gray-700'>Origin: {character.origin.name}</p>
+						<p className='text-gray-700'>Location: {character.location.name}</p>
+					</div>
+
+					<div>
+						<img
+							src={character.image}
+							alt={character.name}
+							className='w-56 rounded-md'
+						/>
+					</div>
+				</div>
+
+				<div>
+					<h3 className='text-xl font-bold'>Episodes:</h3>
+					<ul>
+						{character.episode.map((episode: string, index: number) => (
+							<li key={index} className='text-gray-700'>
+								<Link to={episode} className='text-blue-500' target='_blank'>
+									{episode}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- frontend/src/components/CharacterList.tsx
+++ frontend/src/components/CharacterList.tsx
@@ -1,10 +1,10 @@
 import { type FC, useState } from "react";
 import { useCharacters } from "../hooks/useCharacters";
 import Pagination from "./Pagination";
+import { Character } from "../types";
+import { Link } from "react-router-dom";
 
-const CharacterList: FC<{ onSelectCharacter: (name: string) => void }> = ({
-	onSelectCharacter,
-}) => {
+const CharacterList: FC = () => {
 	const [page, setPage] = useState(1);
 	const { data, isLoading, error } = useCharacters(page);
 
@@ -12,18 +12,23 @@ const CharacterList: FC<{ onSelectCharacter: (name: string) => void }> = ({
 	if (error) return <p className='text-center text-red-500'>Error fetching characters.</p>;
 
 	return (
-		<div className='p-4'>
-			<ul className='grid grid-cols-2 gap-4 md:grid-cols-4'>
-				{data.map((character: { name: string }) => (
-					<li
-						key={character.name}
-						className='p-4 border rounded shadow hover:bg-gray-200 cursor-pointer'
-						onClick={() => onSelectCharacter(character.name)}>
-						{character.name}
-					</li>
-				))}
-			</ul>
-			<Pagination currentPage={page} onPageChange={(newPage) => setPage(newPage)} />
+		<div className='container mx-auto p-4'>
+			<h1 className='text-3xl font-bold text-center mb-6 text-red-500'>Character Explorer</h1>
+			<div className='p-4'>
+				<ul className='grid grid-cols-2 gap-4 md:grid-cols-4 list-none'>
+					{data.map((character: Character) => (
+						<li
+							key={character.id}
+							className='p-4 border rounded shadow hover:bg-gray-200 cursor-pointer'>
+							<Link to={`/characters/${character.name}`}>
+								<img src={character.image} alt='' loading='lazy' />
+								{character.name}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<Pagination currentPage={page} onPageChange={(newPage) => setPage(newPage)} />
+			</div>
 		</div>
 	);
 };
