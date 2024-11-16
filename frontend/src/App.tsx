6c5af13014d80fcac3eb733d36ca525bcf1e4ef8
Aryav <aryavbhola@gmail.com>
2024-11-17 01:14:42 +0530

    created url routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,16 @@
-import { useState, type FC } from "react";
 import CharacterList from "./components/CharacterList";
 import CharacterDetails from "./components/CharacterDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { type FC } from "react";
 
 const App: FC = () => {
-	const [selectedCharacter, setSelectedCharacter] = useState<string | null>(null);
-
 	return (
-		<div className='container mx-auto p-4'>
-			<h1 className='text-3xl font-bold text-center mb-6'>Character Explorer</h1>
-			{!selectedCharacter ? (
-				<CharacterList onSelectCharacter={setSelectedCharacter} />
-			) : (
-				<div>
-					<CharacterDetails characterName={selectedCharacter} />
-					<button
-						onClick={() => setSelectedCharacter(null)}
-						className='mt-4 px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600'>
-						Back to List
-					</button>
-				</div>
-			)}
-		</div>
+		<Router>
+			<Routes>
+				<Route path='/' element={<CharacterList />} />
+				<Route path='/characters/:name' element={<CharacterDetails />} />
+			</Routes>
+		</Router>
 	);
 };
 
